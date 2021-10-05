b5ea4aff6015b93e49032e55baa8b8710c1d4afe
Leonardo Maffei <leobmaffei@gmail.com>
2021-10-04 22:35:02 -0300

    change project path files

--- FoxDesignSystem/generateTagFiles.sh
+++ FoxDesignSystem/generateTagFiles.sh
@@ -1,10 +1,10 @@
 mkdir -p DesignSystem &&
-cat ${PROJECT_DIR}/DesignSystemContent/DesignSystemTokens.swift.gyb > DesignSystem/DesignSystemTokens.swift.gyb &&
+cat ${PODS_ROOT}/FoxDesignSystem/FoxDesignSystem/DesignSystemTokens.swift.gyb > DesignSystem/DesignSystemTokens.swift.gyb &&
 for INFILE in ${!SCRIPT_INPUT_FILE_*};
 do
     file=${!INFILE}
     if [ "${file: -4}" == ".gyb" ]; then
         echo "Processing template $file"
-        "${PROJECT_DIR}/FoxDesignSystem/Scripts/gyb" --line-directive '' -o "${file%.gyb}" "$file"
+        "${PODS_ROOT}/FoxDesignSystem/FoxDesignSystem/gyb" --line-directive '' -o "${file%.gyb}" "$file"
     fi
 done
